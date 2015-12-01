72b60bbf4812d58c5e54ab901fc52f1e0c9e623b
roiti46 <roiti46@gmail.com>
2015-12-02 03:33:23 +0900

    change indent size 4 to 2

--- cpp/bit.h
+++ cpp/bit.h
@@ -6,36 +6,35 @@
 **/
 
 template<class T> class BIT {
-  public:
-    vector<T> bit;
-    int size;
+ public:
+  vector<T> bit;
+  int size;
+  BIT() {}
+  BIT(int n, T v = 0) {
+    size = n;
+    bit.resize(size + 1, v);
+  }
 
-    BIT() {}
-    BIT(int n, T v = 0) {
-        size = n;
-        bit.resize(size + 1, v);
+  // sum [0, i)
+  T sum(int i) {
+    T s = 0;
+    while (i > 0) {
+      s += bit[i];
+      i -= i & -i;
     }
+    return s;
+  }
 
-    // sum [0, i)
-    T sum(int i) {
-        T s = 0;
-        while (i > 0) {
-            s += bit[i];
-            i -= i & -i;
-        }
-        return s;
-    }
+  // sum [i, j)
+  T sum(int i, int j) {
+    return sum(j) - sum(i);
+  }
 
-    // sum [i, j)
-    T sum(int i, int j) {
-        return sum(j) - sum(i);
+  void add(int i, T x) {
+    i++;
+    while (i <= size) {
+      bit[i] += x;
+      i += i & -i;
     }
-
-    void add(int i, T x) {
-        i++;
-        while (i <= size) {
-            bit[i] += x;
-            i += i & -i;
-        }
-    }
+  }
 };
--- cpp/union_find.h
+++ cpp/union_find.h
@@ -5,44 +5,37 @@
 **/
 class UnionFind {
  public:
-    int g_num;
-    vector<int> rank, par;
+  int g_num;
+  vector<int> rank, par;
+  UnionFind() {}
+  UnionFind(int size) {
+    rank.resize(size, 0); par.resize(size, 0);
+    for (int i = 0; i < size; i++) par[i] = i, rank[i] = 0;
+    g_num = size;
+  }
 
-    UnionFind() {}
-    UnionFind(int size) {
-        rank.resize(size, 0);
-        par.resize(size, 0);
-        for (int i = 0; i < size; i++) {
-            par[i] = i;
-            rank[i] = 0;
-        }
-        g_num = size;
+  bool same(int x, int y) {
+    return find(x) == find(y);
+  }
+
+  void unite(int x, int y) {
+    x = find(x); y = find(y);
+    if (x == y) return;
+    g_num--;
+    if (rank[x] > rank[y]) {
+      par[y] = x;
+    } else {
+      par[x] = y;
+      if (rank[x] == rank[y]) rank[y]++;
     }
+  }
 
-    bool same(int x, int y) {
-        return find(x) == find(y);
-    }
+  int find(int x) {
+    if (x == par[x]) return x;
+    return par[x] = find(par[x]);
+  }
 
-    void unite(int x, int y) {
-        x = find(x);
-        y = find(y);
-        if (x == y) return;
-
-        g_num--;
-        if (rank[x] > rank[y]) {
-            par[y] = x;
-        } else {
-            par[x] = y;
-            if (rank[x] == rank[y]) rank[y]++;
-        }
-    }
-
-    int find(int x) {
-        if (x == par[x]) return x;
-        return par[x] = find(par[x]);
-    }
-
-    int group_num() {
-        return g_num;
-    }
+  int group_num() {
+    return g_num;
+  }
 };
